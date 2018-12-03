2597c19d98a96f28a01ca3c125a1bd0ebd3fcac6
rusllan <rus-108@yandex.ru>
2018-12-03 18:18:02 +0500

    :peace_symbol::one::two: some version conflicts in manifests areautomatically resolved

--- pos_qr_scan/__manifest__.py
+++ pos_qr_scan/__manifest__.py
@@ -4,7 +4,7 @@
     "category": "Point of Sale",
     # "live_test_url": "",
     "images": ["images/main.png"],
-    "version": "11.0.1.0.1",
+    "version": "12.0.1.0.1",
     "application": False,
 
     "author": "IT-Projects LLC, KolushovAlexandr",
